72553af6b5f7f41470b78221db922f1733b7c012
paolo9688 <paolo.pz1@gmail.com>
2025-05-12 12:28:21 +0200

    created HeroRepository class

--- src/main/java/com/example/rpg_api_demo/repository/HeroRepository.java
+++ src/main/java/com/example/rpg_api_demo/repository/HeroRepository.java
@@ -1,9 +1,10 @@
 package com.example.rpg_api_demo.repository;
 
+import com.example.rpg_api_demo.model.Hero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Hero, Long> {
+public interface HeroRepository extends JpaRepository<Hero, Long> {
 
 }
